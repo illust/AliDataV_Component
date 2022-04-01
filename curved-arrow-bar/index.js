c26c8e76f1b521d0af8ada91abeca114db9b306d
svenwong <illust0130@foxmail.com>
2022-04-01 17:37:51 +0800

    improve curved-arrow-bar

--- curved-arrow-bar/index.js
+++ curved-arrow-bar/index.js
@@ -53,11 +53,14 @@ module.exports = Event.extend(function Base(container, config) {
       label:{
         normal:{
           show:true,
-          textStyle:{
-            color: '#fff'
-          },
+          color: cfg.options.chart.arrowStyle.arrowFont.color,
+          fontFamily: cfg.options.chart.arrowStyle.arrowFont.fontFamily,
+          fontWeight: cfg.options.chart.arrowStyle.arrowFont.fontWeight,
+          fontSize: cfg.options.chart.arrowStyle.arrowFont.fontSize,
+          // position: [cfg.options.chart.arrowStyle.textPosition.xPosition,cfg.options.chart.arrowStyle.textPosition.xPosition],
+          // distance: cfg.options.chart.arrowStyle.distance,
           formatter:function(params){ //标签内容
-            return  (data.map(e=>e.value)[1]/data.map(e=>e.value)[0]-1).toFixed(2)*100+'%'
+            return  ((data.map(e=>e.value)[1]/data.map(e=>e.value)[0]-1)*100).toFixed(2)+'%'
           },
         }
       }
@@ -67,11 +70,14 @@ module.exports = Event.extend(function Base(container, config) {
       label:{
         normal:{
           show:true,
-          textStyle:{
-            color: '#fff'
-          },
+          color: cfg.options.chart.arrowStyle.arrowFont.color,
+          fontFamily: cfg.options.chart.arrowStyle.arrowFont.fontFamily,
+          fontWeight: cfg.options.chart.arrowStyle.arrowFont.fontWeight,
+          fontSize: cfg.options.chart.arrowStyle.arrowFont.fontSize,
+          // position: [cfg.options.chart.arrowStyle.textPosition.xPosition,cfg.options.chart.arrowStyle.textPosition.xPosition],
+          // distance: cfg.options.chart.arrowStyle.distance,
           formatter:function(params){ //标签内容
-            return  (data.map(e=>e.value)[1]/data.map(e=>e.value)[0]-1).toFixed(2)*100+'%'
+            return  ((data.map(e=>e.value)[1]/data.map(e=>e.value)[0]-1)*100).toFixed(2)+'%'
           }
         }
       }
@@ -158,13 +164,6 @@ module.exports = Event.extend(function Base(container, config) {
         }
       },
       legend: {
-        // top:'10%',
-        // x:'right',
-        // padding:[0,0,10,0],
-        // textStyle:{
-        //   color:cfg.color
-        // },
-
         show:cfg.options.chart.numericalLabel.show,
         x:cfg.options.chart.numericalLabel.pos.horizontal,
         y:cfg.options.chart.numericalLabel.pos.vertical,
@@ -175,11 +174,16 @@ module.exports = Event.extend(function Base(container, config) {
           fontSize:cfg.options.chart.numericalLabel.textStyle.fontSize,
           fontWeight:cfg.options.chart.numericalLabel.textStyle.fontWeight,
           fontFamily:cfg.options.chart.numericalLabel.textStyle.fontFamily
-        },
-        data: ['Arrow', 'Bar']
+        }
+        // ,
+        // data: ['Arrow', 'Bar']
       },
       grid:{
-        top: '35%'  
+        top: cfg.options.chart.margin.top + '%',
+        bottom: cfg.options.chart.margin.bottom + '%',
+        left: cfg.options.chart.margin.left + '%',
+        right: cfg.options.chart.margin.right + '%',
+        containLabel: true
       },
       xAxis: {
         data: data.map(e=>e.name),
@@ -201,7 +205,39 @@ module.exports = Event.extend(function Base(container, config) {
           fontSize: cfg.options.axis.xaxis.label.textarea.fontSize,
           fontWeight: cfg.options.axis.xaxis.label.textarea.fontWeight,
           color: cfg.options.axis.xaxis.label.textarea.color,
-          interval: 0
+          interval: 0,
+          formatter: function(params) {
+            var newParamsName = ""; // 最终拼接成的字符串
+            var paramsNameNumber = params.length; // 实际标签的个数
+            var provideNumber = cfg.options.axis.xaxis.label.textNum; // 每行能显示的字的个数
+            var rowNumber = Math.ceil(paramsNameNumber / provideNumber); // 换行的话，需要显示几行，向上取整
+            /**
+             * 判断标签的个数是否大于规定的个数， 如果大于，则进行换行处理 如果不大于，即等于或小于，就返回原标签
+             */
+            // 条件等同于rowNumber>1
+            if (paramsNameNumber > provideNumber) {
+                /** 循环每一行,p表示行 */
+                for (var p = 0; p < rowNumber; p++) {
+                    var tempStr = ""; // 表示每一次截取的字符串
+                    var start = p * provideNumber; // 开始截取的位置
+                    var end = start + provideNumber; // 结束截取的位置
+                    // 此处特殊处理最后一行的索引值
+                    if (p == rowNumber - 1) {
+                        // 最后一次不换行
+                        tempStr = params.substring(start, paramsNameNumber);
+                    } else {
+                        // 每一次拼接字符串并换行
+                        tempStr = params.substring(start, end) + "\n";
+                    }
+                    newParamsName += tempStr; // 最终拼成的字符串
+                  }
+                } else {
+                  // 将旧标签的值赋给新标签
+                  newParamsName = params;
+                }
+                //将最终的字符串返回
+              return newParamsName;
+          }
         }
       },
       yAxis: [{
@@ -262,18 +298,18 @@ module.exports = Event.extend(function Base(container, config) {
           fontSize: cfg.options.axis.yaxis2.label.textarea.fontSize,
           fontWeight: cfg.options.axis.yaxis2.label.textarea.fontWeight,
           color: cfg.options.axis.yaxis2.label.textarea.color,
-          formatter: '{value}%',
+          formatter: cfg.options.axis.yaxis2.label.percentShow ? '{value} %' : '{value}',
           interval: 0
         }
       }],
       series: [
         {
           type: 'bar',
-          name: 'Bar',
+          name: cfg.options.series.bar.barName,
           // barGap: '30%',
           barCetagoryGap: '30%',
-          barWidth:100,
           data: data.map(e=>e.value),
+          barWidth: cfg.options.series.bar.barPercent,
           itemStyle:{
             color: cfg.options.series.bar.barColor
           },
@@ -284,30 +320,45 @@ module.exports = Event.extend(function Base(container, config) {
               color: cfg.options.series.bar.barText.textStyle.color,
               fontFamily: cfg.options.series.bar.barText.textStyle.fontFamily,
               fontSize: cfg.options.series.bar.barText.textStyle.fontSize,
-              fontWeight: cfg.options.series.bar.barText.textStyle.fontWeight
+              fontWeight: cfg.options.series.bar.barText.textStyle.fontWeight,
+              formatter: cfg.options.series.bar.barText.thousandth? function(p){
+                if (!p.value && p.value !== 0) return 0;
+
+                let str = p.value.toString();
+                let reg =
+                  str.indexOf(".") > -1 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(?:\d{3})+$)/g;
+                return str.replace(reg, "$1,");
+              } : '{c}'
             }
           }
         },
         {
           type: 'line',
-          name: 'Line',
+          name: cfg.options.series.line.lineName,
           // barGap: '30%',
           barCetagoryGap: '30%',
-          barWidth:100,
+          connectNulls: cfg.options.series.line.connectNulls,
+          symbol: cfg.options.series.line.lineSymbol.symbol,
+          symbolSize: cfg.options.series.line.lineSymbol.symbolSize,
+          smooth: cfg.options.series.line.lineSmooth,
           data: data.map(e=>e.value1),
           itemStyle:{
-            // color: cfg.options.series.line.lineColor
+            color: cfg.options.series.line.lineColor
           },
           yAxisIndex: 1,
           label:{
               normal:{
               show: cfg.options.series.line.lineText.show,
+              formatter: cfg.options.axis.yaxis2.label.percentShow ? '{c} %' : '{c}',
               position: cfg.options.series.line.lineText.position,
               color: cfg.options.series.line.lineText.textStyle.color,
               fontFamily: cfg.options.series.line.lineText.textStyle.fontFamily,
               fontSize: cfg.options.series.line.lineText.textStyle.fontSize,
               fontWeight: cfg.options.series.line.lineText.textStyle.fontWeight
             }
+          },
+          lineStyle:{
+            width: cfg.options.series.line.lineWidth
           }
         }
       ]
